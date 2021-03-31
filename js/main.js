dd996c0e2a7dc1dd9c8261dd9220e937cf9baefe
Shuman Denis <romlekus666@gmail.com>
2021-03-31 18:00:58 +0400

    pseudo-quiz

---
+++ js/main.js
@@ -0,0 +1,34 @@
+function result () {
+    //body...
+
+    let score = 0;
+    if (document.getElementById('correct1').checked)
+    {
+        score++;
+    }
+    if (document.getElementById('correct2').checked)
+    {
+        score++;
+    }
+    if (document.getElementById('correct3').checked)
+    {
+        score++;
+    }
+    if (document.getElementById('correct4').checked)
+    {
+        score++;
+    }
+    if (document.getElementById('correct5').checked)
+    {
+        score++;
+    }
+    if (document.getElementById('correct6').checked)
+    {
+        score++;
+    }
+
+    alert('Score:' +score)
+    document.write("Score is:"+score)
+
+
+}
